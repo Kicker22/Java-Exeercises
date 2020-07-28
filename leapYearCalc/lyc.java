371d1aabab8e8592505812347b78cf33615b8481
Matt <mattsully01@gmail.com>
2020-07-28 13:57:09 -0600

    adding leapYear calc

---
+++ leapYearCalc/lyc.java
@@ -0,0 +1,17 @@
+package leapYearCalc;
+
+public class lyc {
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                System.out.println(year + " is a leap year");
+            return true;
+        }else if ( year % 4 == 0) && (year % 100 == 0){
+            
+        }
+        return false;
+
+        
+    }
+}
+        
