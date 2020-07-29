a02540722dc92bf0b2e5938508d7625cd680325b
Matthew <mattsully01@gmail.com>
2020-07-28 22:10:20 -0600

    adding decimal comparator

--- decimalComparator/Main.java
+++ decimalComparator/Main.java
@@ -1,5 +1,7 @@
 package decimalComparator;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        decimalComp.areEqualByThreeDecimalPlaces(3.0, 3.1);
+    }
 }
--- decimalComparator/decimalComp.java
+++ decimalComparator/decimalComp.java
@@ -1,5 +1,14 @@
 package decimalComparator;
 
 public class decimalComp {
-    public static double areEqualByThreeDecimalPlaces(double a, double b)
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+        
+            if ((a - b <= 0.0009 && a - b >= -0.0009)){
+                System.out.println(a + " & " + b + " are equal by three decimal places ");
+                    return true;
+            }
+            System.out.println(a + " & " + b + " are not equal by three decimal places");
+            return false;
+        }
+
 }
